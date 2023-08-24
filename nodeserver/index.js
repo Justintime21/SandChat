ccd37441e30d0abdd36c0ac64b5a5457e083a910
Sandeep Gautam <142665410+Justintime21@users.noreply.github.com>
2023-08-24 03:40:32 -0700

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -8,14 +8,14 @@ const users={};
 
 // this means . when the io is on establish a connection with the socket and run this function 
 io.on('connection', socket =>{
-    // if we get a user-joined event then the user socket id will become its name 
+    // if we get a user-joined event then the user socket id is given to the name. this is done so that when people of the same name join there is no confusion since there socket id will be different
     socket.on('new-user-joined', name1 =>{
         console.log('New-User', name1);
         users[socket.id]=name1;
         // broadcaste.emit will emit(show) all the users except the person who joined
         socket.broadcast.emit('user-joined', name1);
     });
-    // when the send event is done then run this function
+    // when the send/recieve event is done then run this function
     socket.io('send',message =>{
         // broadcaste the message to all except the sender 
         socket.broadcast.emit('receive',{message:message, name1:users[socket.id]})
